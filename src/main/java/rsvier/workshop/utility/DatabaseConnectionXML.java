ace042757a29ee3639bc1f9603a5fb7c2514debd
VLKNK <yehtaryavuz@gmail.com>
2018-03-23 00:50:23 +0100

    changed again

--- src/main/java/rsvier/workshop/utility/DatabaseConnectionXML.java
+++ src/main/java/rsvier/workshop/utility/DatabaseConnectionXML.java
@@ -28,7 +28,7 @@ public class DatabaseConnectionXML {
 		 * missing parts to the new File() and it should find the file. btw don't forget
 		 * to use double backslashes to make it compatible with all Operating Systems.
 		 */
-		File xmlFile = new File("/src/main/java/rsvier/workshop/utility/DCXML.xml");
+		File xmlFile = new File("src/main/java/rsvier/workshop/utility/DCXML.xml");
 
 		if (xmlFile.exists()) {
 
@@ -41,7 +41,7 @@ public class DatabaseConnectionXML {
 				USER = document.getElementsByTagName("user").item(0).getTextContent();
 				PASSWORD = document.getElementsByTagName("password").item(0).getTextContent();
 			} catch (ParserConfigurationException | SAXException | IOException e) {
-				e.printStackTrace();
+				e.getMessage();
 
 			}
 		} else {

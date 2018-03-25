b3ef062bc3e7e0ae861e791e714d4db9e9df9427
VLKNK <yehtaryavuz@gmail.com>
2018-03-26 00:43:05 +0200

    Added 1 new Class variable
    The idea is that this variable holds all orderLines and is important for
    the calculation of the totalPrice of the order.

--- src/main/java/rsvier/workshop/domain/Order.java
+++ src/main/java/rsvier/workshop/domain/Order.java
@@ -2,22 +2,34 @@ package rsvier.workshop.domain;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.*;
 
 public class Order {
 
 	/*
-	 * Order has an id, a customer, a total price and a orderdateTime we are thinking of adding a ArrayList<OrderLine> orderLine;
-	 * to hold all the orderliness that are being made by the customer. not sure if it will work yet we gonna ask jeroen.
+	 * Order has an id, a customer, a total price and a orderdateTime we are
+	 * thinking of adding a ArrayList<OrderLine> orderLine; to hold all the
+	 * orderliness that are being made by the customer. not sure if it will work yet
+	 * we gonna ask jeroen.
 	 * 
 	 */
 
 	private int orderId;
+	private List<OrderLine> totalOrderLines;
+	/*
+	 * Thought of saving all the OrderLines of 1 order inside of this private List.
+	 * Maybe its a ArrayList or LinkedList(Set,Map), When implementing this in the
+	 * DAO the idea is to loop true all orderLines, get the Price add it all to one
+	 * variable an then add that to the TotalPrice in the Order class. Still need to
+	 * think about this, Help me EVA &EVY :D
+	 */
 	private Customer customer;
 	private BigDecimal totalPrice;
 	private LocalDateTime orderDateTime = LocalDateTime.now();
 
 	private Order(OrderBuilder builder) {
 		this.orderId = builder.orderId;
+		this.totalOrderLines = builder.totalOrderLines;
 		this.customer = builder.customer;
 		this.totalPrice = builder.totalPrice;
 		this.orderDateTime = builder.orderDateTime;
@@ -27,6 +39,7 @@ public class Order {
 	public class OrderBuilder {
 
 		private int orderId;
+		private List<OrderLine> totalOrderLines;
 		private Customer customer;
 		private BigDecimal totalPrice;
 		private LocalDateTime orderDateTime = LocalDateTime.now();
@@ -37,6 +50,11 @@ public class Order {
 
 		public OrderBuilder id(int orderId) {
 			this.orderId = orderId;
+			return this;
+		}
+
+		public OrderBuilder totalOrderLines(List<OrderLine> totalOrderLines) {
+			this.totalOrderLines = totalOrderLines;
 			return this;
 		}
 
@@ -64,6 +82,10 @@ public class Order {
 		return orderId;
 	}
 
+	public List<OrderLine> getOrderLine() {
+		return totalOrderLines;
+	}
+
 	public Customer getCustomer() {
 		return customer;
 	}
@@ -80,8 +102,8 @@ public class Order {
 
 	@Override
 	public String toString() {
-		return getOrderId() + " " + getCustomer().getLastName() + getCustomer().getName() + " " + getTotalPrice() + " "
-				+ getOrderDateTime();
+		return getOrderId() + " " + getOrderLine() + getCustomer().getLastName() + getCustomer().getName() + " "
+				+ getTotalPrice() + " " + getOrderDateTime();
 	}
 
 }

0fc573ecaf0eb2f925bc5d9b1730869d7f33c9a6
evywetch <evywetch@gmail.com>
2018-04-03 20:58:05 +0200

    Changed some code in AddressDAO.

--- src/main/java/rsvier/workshop/dao/AddressDAO.java
+++ src/main/java/rsvier/workshop/dao/AddressDAO.java
@@ -1,16 +1,16 @@
 package rsvier.workshop.dao;
 
 import java.util.*;
+import rsvier.workshop.domain.*;
 
-import rsvier.workshop.domain.Address;
-import rsvier.workshop.domain.Customer;
+
 
 public interface AddressDAO {
 
 	public List<Address> getAllAddresses();
-	public Address getAddress(int customerId );
-	public void createAddress(Address address, Customer customer);
-	public void updateAddress(Address address,Customer customer);
-	public void deleteAddress(int customerId);
+	public Address getAddress(int personId );
+	public void createAddress(Address address, Person person);
+	public void updateAddress(Address address,Person person);
+	public void deleteAddress(Person person);
 }
 

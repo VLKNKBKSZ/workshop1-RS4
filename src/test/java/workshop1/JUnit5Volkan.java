09bf26f19e993fcaf45d676a1914340a27f19231
VLKNK <yehtaryavuz@gmail.com>
2018-04-11 01:54:00 +0200

    created automated Junit5Test

---
+++ src/test/java/workshop1/JUnit5Volkan.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package workshop1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Volkan Kabaksiz
+ *
+ */
+class JUnit5Volkan {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

55c674ed55f48af4930e96561c58fc596b32da54
VLKNK <yehtaryavuz@gmail.com>
2018-04-11 20:58:21 +0200

    Added more methods to the ProductsDAOImpTest.

--- src/test/java/workshop1/ProductDAOImpTest.java
+++ src/test/java/workshop1/ProductDAOImpTest.java
@@ -1,19 +1,15 @@
-/**
- * 
- */
 package workshop1;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.math.BigDecimal;
+import java.math.*;
 import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.*;
+import java.util.logging.*;
 import org.junit.jupiter.api.*;
 import rsvier.workshop.dao.ProductDAOImp;
 import rsvier.workshop.domain.Product;
-import rsvier.workshop.utility.DatabaseConnectionXML;
-import rsvier.workshop.utility.LogConnection;
+import rsvier.workshop.utility.*;
 
 /**
  * @author Volkan Kabaksiz
@@ -27,15 +23,16 @@ class ProductDAOImpTest {
 	@BeforeEach
 	void setUp() {
 
-		try {
+		try (Connection conn = DatabaseConnectionXML.getConnection(); Statement statement = conn.createStatement();) {
 
-			Connection conn = DatabaseConnectionXML.getConnection();
-			Statement statement = conn.createStatement();
-			String query = "INSER INTRO product (name, price , stock) VALUES ('Tafel',95.00, 25)";
+			String query = "INSERT INTO product (name, price , stock) VALUES ('Tafel',95.00, 25)";
+			String query_2 = "INSERT INTO product (name, price , stock) VALUES ('Lamp',77.00, 77)";
+			String query_3 = "INSERT INTO product (name, price , stock) VALUES ('Hoekbank',77.00, 77)";
 			statement.addBatch(query);
+			statement.addBatch(query_2);
+			statement.addBatch(query_3);
 			statement.executeBatch();
 			logger.log(Level.INFO, "Creation of dummy product is successfull");
-			conn.close();
 
 		} catch (SQLException e) {
 			logger.log(Level.WARNING, "SQL exception occured", e);
@@ -47,9 +44,8 @@ class ProductDAOImpTest {
 	@AfterEach
 
 	void tearDown() {
-		try {
-			Connection conn = DatabaseConnectionXML.getConnection();
-			Statement statement = conn.createStatement();
+		try (Connection conn = DatabaseConnectionXML.getConnection(); Statement statement = conn.createStatement();) {
+
 			String query = "DELETE FROM product;";
 			String query_2 = "ALTER TABLE product AUTO_INCREMENT = 1";
 			statement.addBatch(query);
@@ -65,14 +61,41 @@ class ProductDAOImpTest {
 
 	@Test
 	void canCreateProduct() {
-		Product product = new Product.ProductBuilder().name("Stoel").price(new BigDecimal("999.00")).stock(99)
-				.build();
+		Product product = new Product.ProductBuilder().name("Stoel").price(new BigDecimal("999.00")).stock(99).build();
 		productDAO.createProduct(product);
 		Product productReturned = productDAO.getProductByName("Stoel");
 		String name = productReturned.getName();
-
 		assertEquals("Stoel", name);
+	}
 
+	@Test
+	void canGetProductById() {
+		Product product = productDAO.getProductById(1);
+		assertEquals("Tafel", product.getName());
+	}
+
+	@Test
+	void canGetProductByName() {
+		Product product = productDAO.getProductByName("Lamp");
+		assertEquals("Lamp", product.getName());
+	}
+
+	@Test
+	void canDeleteProduct() {
+		Product product = new Product.ProductBuilder().name("Stoel").price(new BigDecimal("999.00")).stock(99).build();
+		productDAO.createProduct(product);
+		productDAO.deleteProduct(productDAO.getProductByName("Stoel"));
+		assertNull("This should return null", productDAO.getProductByName("Stoel"));
+	}
+
+	@Test
+	void canGetAllProducts() {
+		List<Product> listOfProducts = productDAO.getAllProducts();
+		assertAll("listOfProducts", () -> assertEquals(3, listOfProducts.size()),
+				() -> assertEquals("Tafel", listOfProducts.get(0).getName()),
+				() -> assertNotNull(listOfProducts.get(1).getName())
+
+		);
 	}
 
 }

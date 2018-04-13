e629c38602cc2ea85b2e0bd664707b2a99a19125
VLKNK <yehtaryavuz@gmail.com>
2018-04-13 09:20:41 +0200

    Created a OrderLineDAOImpTest, not functional yet and some cleane up

---
+++ src/test/java/workshop1/OrderLineDAOImpTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package workshop1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author kabak
+ *
+ */
+class OrderLineDAOImpTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

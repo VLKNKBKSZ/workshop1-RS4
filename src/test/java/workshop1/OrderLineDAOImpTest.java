584cc271fe792aa9cd01407c8736a3ba89594b80
VLKNK <yehtaryavuz@gmail.com>
2018-04-13 09:21:10 +0200

    Created new OrderLineDAOImpTest not functional yet

--- src/test/java/workshop1/OrderLineDAOImpTest.java
+++ src/test/java/workshop1/OrderLineDAOImpTest.java
@@ -1,32 +1,45 @@
-/**
- * 
- */
 package workshop1;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.sql.*;
+import java.util.logging.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import rsvier.workshop.dao.AccountDAOImp;
+import rsvier.workshop.dao.OrderDAOImp;
+import rsvier.workshop.dao.OrderLineDAOImp;
+import rsvier.workshop.dao.PersonDAOImp;
+import rsvier.workshop.dao.ProductDAOImp;
+import rsvier.workshop.utility.DatabaseConnectionXML;
+import rsvier.workshop.utility.LogConnection;
+
 /**
- * @author kabak
+ * @author Volkan Kabaksiz
  *
  */
 class OrderLineDAOImpTest {
+	
+	private Logger logger = LogConnection.getLogger();
+	
+	AccountDAOImp accountDAO = new AccountDAOImp();
+	PersonDAOImp personDAO = new PersonDAOImp();
+	ProductDAOImp productDAO = new ProductDAOImp();
+	OrderDAOImp orderDAO = new OrderDAOImp();
+	OrderLineDAOImp orderLineDAO = new OrderLineDAOImp();
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@BeforeEach
-	void setUp() throws Exception {
+	void setUp() {
+		try (Connection conn = DatabaseConnectionXML.getConnection(); Statement statement = conn.createStatement();) {
+
+		} catch (SQLException e) {
+			logger.log(Level.WARNING, "SQL Exception occured",e);
+		}
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@AfterEach
-	void tearDown() throws Exception {
+	void tearDown() {
 	}
 
 	@Test

db50a0cde127f4cf78245fc36c6795534ccc4b12
evywetch <evywetch@gmail.com>
2018-04-16 10:11:54 +0200

    Fixed AccountDAOTest. It's now green.

--- src/main/java/rsvier/workshop/TestEvy.java
+++ src/main/java/rsvier/workshop/TestEvy.java
@@ -43,54 +43,201 @@ public class TestEvy {
 		addressDao.createAddress(address1, person);
 		
 		*/
+		
+		
 
 		// Person 2
 		
 		
 
-		Account account2 = new Account("appie@gmail.com", "678910");
-	//	accountDao.createAccount(account2);
-		Account accountDatabse2 = accountDao.getAccount(account2.getEmail(), account2.getPassword());
+		//	Account account2 = new Account("appie@gmail.com", "678910");
+// @Test 1 Create account = successful
+		//	accountDao.createAccount(account2);
+		
+// @Test  Get account  = successful
+		Account accountDatabse2 = accountDao.getAccount("appie@gmail.com", "letappiein");
+			//System.out.println(accountDatabse2.toString());
+	
+		
+// @Test  Get all account  = successful
+		/*
+				ArrayList<Account> accountList = (ArrayList) accountDao.getAllAccounts();
+		
 
+			for(int i = 0 ; i < accountList.size() ; i++){
+				System.out.println(accountList.get(i).toString());
+	
+			}
+			
+		*/
+	
+		
+// @Test Update account  =  successful
+		
+	//	accountDatabse2.setPassword("letappiein");
+	//	accountDao.updateAccount(accountDatabse2);
+		
+		
+// @Test Delete account  = NOT successful coz the foreign key problem		
+		
+		accountDao.deleteAccount(accountDatabse2);
+		
+		
 		Person person2 = (Person) new Person.PersonBuilder().accountId(accountDatabse2.getAccountId()).personType("employee")
-				.name("appie").lastName("pope").build();
-	//	personDao.createPerson(person2);
-		Person personDatabase2 = personDao.getPersonById(person2.getAccountId());
-
-		Address address2 = new Address.AddressBuilder().streetName("Street").houseNumber(111).postalCode("2000ME")
-				.city("Rotterdam").country("Netherlands").build();
-	//	addressDao.createAddress(address2,personDatabase2);
-		
-		
-	//	Product product = new Product.ProductBuilder().name("stole").price(new BigDecimal("100")).stock(5).build();
-		
+			.name("appie").lastName("pope").build();
+// @Test Create person = successful
+		//	personDao.createPerson(person2);
 		
 	
-//		productDao.createProduct(product);
 		
-	//	Product productDatabase = productDao.getProductByName("stole");
+// @Test Get personByLastName & ById & getAddress = successful
+		//Person personDatabase2 = personDao.getPersonByLastName("pope");
 		
+		//System.out.println(personDatabase2.toString());
 		
-		Order order = new Order.OrderBuilder().build();
-	//	orderDao.createOrder(order, personDatabase2);
+// @Test Get all persons = successful
 		
-	//	Order orderDatabse = orderDao.getOrderById(1);
+		/*	ArrayList personList = (ArrayList) personDao.getAllPersons();
 		
-	//	OrderLine orderLine = new OrderLine.OrderLineBuilder().order(orderDatabse).product(productDatabase).build();
+		for(int i = 0 ; i  < personList.size(); i++){
+			System.out.println(personList.get(i).toString());
+		}
 		
-	//	orderLineDao.createOrderLine(orderLine);
+		 */
 		
+// @Test Update person = successful 
 		
-	//	addressDao.deleteAddress(personDatabase2.getPersonId());
-		
-		personDao.deletePerson(personDatabase2);
-		
-		
-		
-		
+	//	Person updatePerson = new Person.PersonBuilder(personDatabase2).middleName("middle").build();
+	//	personDao.updatePerson(updatePerson);
 		
 		
+			
+// @Test Delete person without delete account = not successful
+		
+		//	personDao.deletePerson(personDatabase2);
+
+			Address address2 = new Address.AddressBuilder().streetName("Street").houseNumber(111).postalCode("2000ME")
+					           .city("Rotterdam").country("Netherlands").build();
+		
+// @Test Create address = successful
+			//addressDao.createAddress(address2,personDatabase2.getPersonId());
+		
+// @Test Delete address without delete person = successful
+		
+		//	addressDao.deleteAddress(personDatabase2.getPersonId());
+		
+
+// @Test Get all address = successful	
+		
+		//ArrayList addressList =  (ArrayList<Address>) addressDao.getAllAddresses();
 		
 		
+		
+	/*	for (int i = 0 ; i < addressList.size(); i++){
+			System.out.println(addressList.get(i).toString());
+		}
+		
+	*/	
+		
+// @Test update address = successful
+	
+		 /*
+		 Address address = personDatabase2.getAddress();
+		 Address updateAddress = new Address.AddressBuilder(address).streetName("J.B.Bakamakade").additionalHouseNumber(3).build();
+		addressDao.updateAddress(updateAddress);
+		*/
+		 
+		 
+		 
+		//	Product product1 = new Product.ProductBuilder().name("stole").price(new BigDecimal("100")).stock(5).build();
+		 
+// @Test Create product = successful. Give price = 95.95 --> price in DB = 96
+		
+		//Product product2 = new Product.ProductBuilder().name("tafel").price(new BigDecimal("95.95")).stock(100).build();
+		
+		//productDao.createProduct(product2);
+		
+// @Test Get product by name = successful	
+		
+		//Product productDatabase = productDao.getProductByName("tafel");
+		//System.out.println(productDatabase.toString());
+		
+// @Test Get all product = successful
+		
+		/*
+		ArrayList productList = (ArrayList) productDao.getAllProducts();
+		
+		for(int i = 0 ; i <productList.size();i++){
+			System.out.println(productList.get(i).toString());
+		}
+		*/
+		
+// @Test Update product = successful	
+		
+		//Product productUpdate = new Product.ProductBuilder(productDatabase).stock(55).build();
+		//productDao.updateProduct(productUpdate);
+		
+// @Test create order = successful	
+		
+	//	Order order = new Order.OrderBuilder().build();
+	//int orderId = orderDao.createOrder(order, personDatabase2);
+	//System.out.println(orderId);
+	
+// @Test Get order by ID = successful
+		//Order orderDatabase = orderDao.getOrderById(3);
+		//System.out.println(orderDatabase.toString());
+		
+		
+// @Test Update order = NOT successful  : Can't update person_id
+		
+		//Order orderUpdate = new Order.OrderBuilder(orderDatabase).person(personDatabase2).build();
+		//orderDao.updateOrder(orderUpdate);
+		
+// @Test Get all orders = successful
+	/*	
+		ArrayList orderList = (ArrayList) orderDao.getAllOrders();
+		for(int i = 0 ; i < orderList.size() ; i++){
+			System.out.println(orderList.get(i).toString());
+		}
+		*/
+
+// @Test Create orderLine = successful
+
+		//OrderLine orderLine = new OrderLine.OrderLineBuilder().order(orderDatabase).product(productDatabase).build();
+		
+		//orderLineDao.createOrderLine(orderLine);
+		
+		
+// @Test Get all orderLine from product = successful : Coz change column order in orderLine table to match column order in the code
+		
+		/* ArrayList orderLineList = (ArrayList) orderLineDao.getAllOrderLinesFromProduct(productDatabase);
+		
+		for(int i = 0; i < orderLineList.size(); i++){
+			System.out.println(((OrderLine) orderLineList.get(i)).toString());
+		
+		}
+		
+		*/
+		
+// @Test Get all orderLine from order = successful : Coz change column order in orderLine table to match column order in the code
+		
+	/*	ArrayList orderLineList = (ArrayList) orderLineDao.getAllOrderLinesFromOrder(orderDatabase);
+		
+		for(int i = 0; i < orderLineList.size(); i++){
+			System.out.println(((OrderLine) orderLineList.get(i)).toString());
+		
+		}
+	*/
+		
+// @Test Get  orderLine = successful
+		
+		//OrderLine orderLineDatabase = orderLineDao.getOrderLine(1);
+		//System.out.println((orderLineDatabase.toString()));
+		
+// @Test Update  orderLine  = NOT successful : String query = wrong : No " SET number_of_products = ?
+		
+		//OrderLine orderLineUpdate = new OrderLine.OrderLineBuilder(orderLineDatabase).numberOfProducts(2).build();
+		//System.out.println((orderLineUpdate.getNumber()));
+		//orderLineDao.updateOrderLine(orderLineUpdate);
 	}
 }

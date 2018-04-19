f6974cc95526a067ba398dc56236934865491d98
VLKNK <dryadengaming@gmail.com>
2018-04-19 15:14:14 +0200

    its now working

--- src/main/java/rsvier/workshop/App.java
+++ src/main/java/rsvier/workshop/App.java
@@ -1,14 +1,7 @@
 package rsvier.workshop;
 
-import rsvier.workshop.controller.MainMenuController;
-
 public class App {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		MainMenuController mainMenuController = new MainMenuController();
-		mainMenuController.doLoginMenu();
-=======
 
->>>>>>> branch 'develop' of https://github.com/VLKNK/workshop1-RS4.git
 	}
 }

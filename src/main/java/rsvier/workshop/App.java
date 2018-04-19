13146fefc6172365cca048df07ae7e3d8a6a0f02
VLKNK <dryadengaming@gmail.com>
2018-04-19 15:13:03 +0200

    added basic

--- src/main/java/rsvier/workshop/App.java
+++ src/main/java/rsvier/workshop/App.java
@@ -1,6 +1,10 @@
 package rsvier.workshop;
 
+import rsvier.workshop.controller.MainMenuController;
+
 public class App {
 	public static void main(String[] args) {
+		MainMenuController mainMenuController = new MainMenuController();
+		mainMenuController.doLoginMenu();
 	}
 }

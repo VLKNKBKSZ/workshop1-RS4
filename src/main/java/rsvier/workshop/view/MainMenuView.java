3532e3846609861444288924520010f305c26562
evywetch <evywetch@gmail.com>
2018-05-03 11:09:03 +0200

    Edited Evvo to Nevvo.

--- src/main/java/rsvier/workshop/view/MainMenuView.java
+++ src/main/java/rsvier/workshop/view/MainMenuView.java
@@ -5,14 +5,14 @@ public class MainMenuView extends View {
 
 	@Override
 	public void printHeaderMessage() {
-		System.out.println("\n=========== Welkom Op Evvo Meubels ===========\n");
+		System.out.println("\n=========== Welkom Op Nevvo Meubels ===========\n");
 		
 	}
 
 	@Override
 	public void printMenuMessage() {
 
-		System.out.println("\n1- Inloggen\n2- Maak een nieuw Account\n0- Verlaat Evvo Meubels");
+		System.out.println("\n1- Inloggen\n2- Maak een nieuw Account\n0- Verlaat Nevvo Meubels");
 	}
 	
 	

97bada04dfd351e8386f121aec46e1707509cd28
= <=>
2018-05-07 16:51:25 +0200

    Orderlines and orders

--- src/main/java/rsvier/workshop/controller/OrderLineController.java
+++ src/main/java/rsvier/workshop/controller/OrderLineController.java
@@ -24,33 +24,31 @@ public class OrderLineController extends Controller {
 
     public void orderLineMenuSwitch(Order order) {
     		
-    		orderLineView.printHeaderMessage();
-    		int menuChoice = orderLineView.getIntInput();
-    		
     		boolean placeOrderOrCancelOrder = true;
     		
     		while(placeOrderOrCancelOrder){
     			
+    			orderLineView.printHeaderMessage();
+            int menuChoice = orderLineView.getIntInput();
+    			
     			switch(menuChoice) {
     			
-    			case 1: //Add product to order
-    					
+    			case 1: //Add orderline (product and amount of products) to order
     					addOrderLineToOrder(order);
     					
     					//what product do you want to add?
     					break;
     		
     			case 2: //View current order
-    					
-    					for (OrderLine orderLine: order.getOrderLine()) {
-    						System.out.println("\n" + orderLine.toString());
-    					}
+    					viewAllOrderlinesInCurrentOrder();
+    					showTotalPriceOfCurrentOrder();
     					break;
     					
-    			case 3: //Place order
-    					//method to place the order
+    			case 3: //Place order. Method to place the order in the database
+    					
     					orderDAO.createOrder(order);
     					orderView.printOrderHasBeenPlaced();
+    					
     					placeOrderOrCancelOrder = false;
     					break;
     					
@@ -75,7 +73,7 @@ public class OrderLineController extends Controller {
     		if (retrievedProduct != null) {
     			System.out.println("\n" + retrievedProduct.toString() + "\n");
     			
-    			//create an orderline 
+    			//Create an orderline (products and how many of them)
     			OrderLine.OrderLineBuilder orderLine = new OrderLine.OrderLineBuilder().product(retrievedProduct);
     			orderLine.numberOfProducts(requestAmountOfProducts());
     			
@@ -98,5 +96,17 @@ public class OrderLineController extends Controller {
     		return amountOfProducts;
     }
     
+    //Method for viewing all the orderlines in the current order
+    public void viewAllOrderlinesInCurrentOrder() {
+    		
+    		for (OrderLine orderLine: order.getOrderLine()) {
+			System.out.println("\n" + orderLine.toString());
+    		}
+    }
+    
+    //Method for showing total price
+    public void showTotalPriceOfCurrentOrder() {
+    	
+    }
     
 }

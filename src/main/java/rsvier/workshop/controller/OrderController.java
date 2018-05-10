79a1cad6d9630d66595efa9edd3d500d3539c87d
VLKNK <dryadengaming@gmail.com>
2018-05-10 17:44:20 +0200

    deleted all the changes and method names

--- src/main/java/rsvier/workshop/controller/OrderController.java
+++ src/main/java/rsvier/workshop/controller/OrderController.java
@@ -15,7 +15,6 @@ public class OrderController extends Controller {
 	private OrderLineController orderLineController = new OrderLineController();
 	private CustomerView customerView = new CustomerView();
 
-	
 	@Override
 	public void runView() {
 		orderView.printHeaderMessage();
@@ -23,21 +22,15 @@ public class OrderController extends Controller {
 		searchCreateOrderMenuSwitch(orderView.getIntInput());
 	}
 
-	
 	public void searchCreateOrderMenuSwitch(int menuNumber) {
 
 		switch (menuNumber) {
 
 		case 1: // Search order
-<<<<<<< HEAD
+
 			selectOrderSearchMenuSwitch();
 			break;
 
-=======
-				searchOrderSwitch();
-				break;
-				
->>>>>>> branch 'develop' of https://github.com/VLKNK/workshop1-RS4.git
 		case 2: // Create order for a customer
 			doCreateOrder((customerController.searchCustomerByLastName()));
 			break;
@@ -53,9 +46,8 @@ public class OrderController extends Controller {
 		}
 	}
 
-	
-	//Search order by order-id or customer last name
-	
+	// Search order by order-id or customer last name
+
 	public void searchOrderSwitch() {
 
 		orderView.printAskSearchOrderByNumberOrByName();
@@ -63,29 +55,27 @@ public class OrderController extends Controller {
 
 		switch (menuNumber) {
 		case 1:// Search order by order ID
-				updateOrDeleteOrderSwitch(searchOrderByOrderId());
-				break;
-				
+			updateOrDeleteOrderSwitch(searchOrderByOrderId());
+			break;
+
 		case 2:// Search order by customer last name
-				updateOrDeleteOrderSwitch(searchOrderByLastName(customerController.searchCustomerByLastName()));
-				break;
-				
+			updateOrDeleteOrderSwitch(searchOrderByLastName(customerController.searchCustomerByLastName()));
+			break;
+
 		case 0: // Back to previous menu
-				runView();
-				break;
-				
+			runView();
+			break;
+
 		default: // Back to this same menu
-				orderView.printMenuInputIsWrong();
-				searchOrderSwitch();
-				break;
+			orderView.printMenuInputIsWrong();
+			searchOrderSwitch();
+			break;
 		}
 	}
-	
-	
-	public void updateOrDeleteOrderSwitch(Order order) {
-<<<<<<< HEAD
 
-		orderView.printAskUserToUpdateOrDeleteProduct();
+	public void updateOrDeleteOrderSwitch(Order order) {
+
+		orderView.printAskUserToUpdateOrDeleteOrder();
 		int menuNumber = orderView.getIntInput();
 
 		switch (menuNumber) {
@@ -105,43 +95,8 @@ public class OrderController extends Controller {
 			runView();
 
 		}
-=======
-		
-		//Check if given order id exists in database, else go back to menu
-		do {
-			orderView.printOrderNotFound();
-			searchOrderSwitch();
-			}
-			while(order == null);
-			
-		//Print the orderlines in the order
-		for (OrderLine orderLine: order.getTotalOrderLines()) {
-				
-			System.out.println(orderLine.toString());
-		}
 
-		orderView.printAskUserToUpdateOrDeleteOrder();
-		int menuNumber = orderView.getIntInput();
-
-		switch (menuNumber) {
-
-				case 1:// Update order
-						updateExistingOrderSwitch(order);
-						break;
-						
-				case 2: // Delete order
-						orderDao.deleteOrder(order);
-						break;
-
-				case 0:// Go back to previous Menu
-
-				default:
-					orderView.printMenuInputIsWrong();
-					runView();
-		}	
->>>>>>> branch 'develop' of https://github.com/VLKNK/workshop1-RS4.git
 	}
-<<<<<<< HEAD
 
 	public void updateExistingOrder(Order order) {
 
@@ -154,74 +109,6 @@ public class OrderController extends Controller {
 		case 1: // Go to orderLine
 
 			orderLineController.viewAllOrderLinesInCurrentOrder(order);
-=======
-	
-	
-	// Search order by OrderId
-	public Order searchOrderByOrderId() {
-
-		orderView.printAskOrderId();
-
-		return orderDao.getOrderById(orderView.getIntInput());
-	}
-		
-	
-
-	// Search order by Last name
-	public Order searchOrderByLastName(Person person) {
-
-		List<Order> orderList = new ArrayList<>();
-
-		if (person == null) {
-
-			customerView.printCustomerNotFound();
-			searchOrderSwitch();
-
-		} else {
-
-			orderList = orderDao.getAllOrdersFromPerson(person);
-
-			if (orderList.size() == 0) {
-
-				// print "Geen bestellingen gevonden"
-				return null;
-			}
-
-			if (orderList.size() == 1) {
-
-				orderView.printOrdersFound(orderList.get(0));
-
-				return orderList.get(0);
-
-			} else {
-
-				for (int i = 1; i < orderList.size(); i++) {
-					orderView.printOrdersFound(orderList.get(i - 1));
-				}
-			}
-		}
-
-		return orderList.get(selectOrder() - 1);
-	}
-
-		
-	
-	public int selectOrder() {
-
-		orderView.printAskSelectOrder();
-
-		return orderView.getIntInput();
-	}
-
-	
-	
-	public void updateExistingOrderSwitch(Order order) {
-		
-		Boolean updating = true;
-		while (updating) {
->>>>>>> branch 'develop' of https://github.com/VLKNK/workshop1-RS4.git
-			
-<<<<<<< HEAD
 
 			break;
 
@@ -237,35 +124,7 @@ public class OrderController extends Controller {
 
 		default:
 			break;
-=======
-			orderView.printUpdateExistingOrder();
-			int menuNumber = orderView.getIntInput();
-		
-			switch(menuNumber) {
-		
-			case 1: //Go to orderLine
-					orderLineController.viewAllOrderLinesInCurrentOrder(order);
-				
-					break;
-				
-			case 2: //Add orderLines to order
-					orderLineController.addOrderLineToOrder(order);
-					break;
-			
-			case 3: //Delete order
-					break;
-		
-			case 4: //Save changes in the database
-				
-					updating = false;
-					break;
-					
-			case 0: updating = false;
-					break;
-			default: 
-					break;
-			}
->>>>>>> branch 'develop' of https://github.com/VLKNK/workshop1-RS4.git
+
 		}
 
 	}
@@ -286,7 +145,6 @@ public class OrderController extends Controller {
 
 	}
 
-<<<<<<< HEAD
 	public void selectOrderSearchMenuSwitch() {
 
 		orderView.printAskSearchOrderByNumberOrByName();
@@ -378,9 +236,6 @@ public class OrderController extends Controller {
 
 	}
 
-=======
-	
->>>>>>> branch 'develop' of https://github.com/VLKNK/workshop1-RS4.git
 	public void doCreateOrder(Person person) {
 
 		// check if person was found
@@ -397,19 +252,8 @@ public class OrderController extends Controller {
 
 			// Pass the order to the switch in the orderLineController
 			orderLineController.orderLineMenuSwitch(order);
-<<<<<<< HEAD
 
-=======
-			
->>>>>>> branch 'develop' of https://github.com/VLKNK/workshop1-RS4.git
 		}
-<<<<<<< HEAD
 
 	}
-=======
-		
-}
-	
->>>>>>> branch 'develop' of https://github.com/VLKNK/workshop1-RS4.git
-
 }

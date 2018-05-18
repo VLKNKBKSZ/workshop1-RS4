83f3117333f8b1be5b523323b82668385c5c05d4
evywetch <evywetch@gmail.com>
2018-05-18 23:59:33 +0200

    The same app is back!

--- src/main/java/rsvier/workshop/App.java
+++ src/main/java/rsvier/workshop/App.java
@@ -14,7 +14,6 @@ public class App {
 	public static void main(String[] args) {
 		
 		View view = new MainMenuView();
-		
 		hikariEnabled = view.printAskUserToEnableHikariOrNot();
 		view.printAskUserToUseSQLOrMongo();
 		
